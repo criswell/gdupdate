99223364011f5dd8a1681c59faa154d41455563a
Sam Hart <hartsn@gmail.com>
2016-03-20 21:12:32 -0400

    begin tests

--- update.py
+++ update.py
@@ -3,8 +3,9 @@
 from __future__ import print_function
 import json
 import sys
-import requests
 import argparse
+import requests
+from godaddypy import Client, Account
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config", help="The JSON config file to load.")
@@ -14,7 +15,31 @@ config = {}
 with open(args.config, 'r') as f:
     config = json.load(f)
 
-if not config.has_key('ip_id'):
+if 'ip_url' not in config or \
+        'key' not in config or \
+        'domain' not in config or \
+        'sub-domain' not in config or \
+        'secret_key' not in config:
     print("Error parsing config file!")
     print(config)
     sys.exit(1)
+
+r = requests.get(config['ip_url'])
+ip_info = r.json()
+
+if 'ip' not in ip_info:
+    print("Error getting IP from 'ip_url'!")
+    print(ip_info)
+    sys.exit(1)
+
+my_ip = ip_info['ip']
+
+my_acct = Account(api_key=config['key'], api_secret=config['secret_key'])
+client = Client(my_acct)
+
+domain = config['domain']
+sub = config['sub-domain']
+
+print(client.update_ip(my_ip, domains=[domain], subdomains=[sub]))
+
+print(client.get_a_records(domain))
