03832b83d4e15c6d6ea9d515cba0947de792b478
Sam Hart <sam.hart@angieslist.com>
2016-03-22 11:46:08 -0400

    flesh out the udpdater

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import json
 import sys
 import argparse
-import requests
+from datetime import datetime, timedelta
 from godaddypy import Client, Account
 
 parser = argparse.ArgumentParser()
@@ -19,6 +19,7 @@ if 'ip_url' not in config or \
         'key' not in config or \
         'domain' not in config or \
         'sub-domain' not in config or \
+        'refresh_time' not in config or \
         'secret_key' not in config:
     print("Error parsing config file!")
     print(config)
@@ -34,12 +35,48 @@ if 'ip' not in ip_info:
 
 my_ip = ip_info['ip']
 
-my_acct = Account(api_key=config['key'], api_secret=config['secret_key'])
-client = Client(my_acct)
+# Note: Here we only concern ourselves with days, hours, seconds, and weeks
+dt = timedelta(
+        days=config['refresh_time'].get('days', 0),
+        hours=config['refresh_time'].get('hours', 0),
+        seconds=config['refresh_time'].get('seconds', 0)
+        weeks=config['refresh_time'].get('weeks', 0)
+        )
 
-domain = config['domain']
-sub = config['sub-domain']
+now = datetime.today()
 
-print(client.update_ip(my_ip, domains=[domain], subdomains=[sub]))
+update_now = False
 
-print(client.get_a_records(domain))
+if 'last_update' in config:
+    last_update = datetime.strptime(config['last_update'], '%Y-%m-%d %H:%M:%S.%f')
+    if now - last_update > dt:
+        update_now = True
+else:
+    update_now = True
+
+if 'last_ip' in config:
+    if my_ip != config['last_ip']:
+        update_now = True
+
+if update_now:
+    my_acct = Account(api_key=config['key'], api_secret=config['secret_key'])
+    client = Client(my_acct)
+
+    domain = config['domain']
+    sub = config['sub-domain']
+
+    print(now.strftime('%Y-%m-%d %H:%M:%S.%f'))
+    if not client.update_ip(my_ip, domains=[domain], subdomains=[sub]):
+        print("ERROR UPDATING DOMAIN!")
+    else:
+        print("Domain updated successfully!")
+
+    print("Current domain info:")
+    print("\t{0}".format(client.get_a_records(domain)))
+
+    config['last_update'] = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+    config['last_ip'] = my_ip
+
+    with open(args.config, 'w') as f:
+        json.dump(config, f)
+
