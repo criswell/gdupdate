fbd65dae6a287ac815c2b5c87ed42f604a3a7139
Sam Hart <hartsn@gmail.com>
2016-03-22 12:36:25 -0400

    add sample config

---
+++ sample-config.js
@@ -0,0 +1,18 @@
+{
+  // This is the base domain to update
+  "domain": "foobar.net",
+  // This is the refresh time
+  "refresh_time": {
+    "hours": 12
+  }, 
+  // This is the sub-domain to update, e.g., update ssh.foobar.net in this
+  // example
+  "sub-domain": "ssh", 
+  // This is the main godaddy API key
+  "key": "huehuehuehuehuehuehuehueheuheuh", 
+  // This is some URL that, when called, returns a JSON object with 'ip' as an
+  // element which contains your IP
+  "ip_url": "http://someplace.somewhere.somehow.com/", 
+  // This is the secret godaddy API key
+  "secret_key": "lelelelelelelelelelelelelelel"
+}
